a318d4c32c7d99ddc4493506741887cdb4171361
RayFFH <phoenix222smart@gmail.com>
2020-09-02 23:12:13 +0100

    Update finance2.py

--- finance2.py
+++ finance2.py
@@ -1,14 +1,22 @@
 import bs4 as bs
 import pickle
 import requests
+import os
+import pandas as pd
+import datetime as dt
+import pandas_datareader.data as web
+
 
 def save_sp500_tickers():
-    resp = requests.get('https://en.wikipedia.org/wiki/S%26P_500_Index')
-    soup = bs.BeautifulSoup(resp.txt)
-    table = soup.find('table',{'class':'wiki table sortable'})
+    resp = requests.get('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')
+    soup = bs.BeautifulSoup(resp.text, "lxml")
+    table = soup.find('table', {'class':'wikitable sortable'})
     tickers = []
     for row in table.findAll('tr')[1:]:
-        ticker = row.findAll('td')[0].text
+        ticker = row.findAll('td')[0].text.replace('\n', '')
+        if "." in ticker:
+            ticker = ticker.replace('.', '-')
+            print('ticker replaced to', ticker)
         tickers.append(ticker)
 
     with open("sp500tickers.pickle","wb") as f:
@@ -18,4 +26,29 @@ def save_sp500_tickers():
 
     return tickers
 
-save_sp500_tickers()
+
+# save_sp500_tickers()
+
+
+def get_data_from_yahoo(reload_sp500=False):
+    if reload_sp500:
+        tickers = save_sp500_tickers()
+    else:
+        with open("sp500tickers.pickle","rb") as f:
+            tickers = pickle.load(f)
+
+    if not os.path.exists('stock_dfs'):
+        os.makedirs('stock_dfs')
+
+    start = dt.datetime(2000,1,1)
+    end = dt.datetime(2020, 9, 2)
+
+    for ticker in tickers:
+        if not os.path.exists('stock_dfs/{}.csv'.format(ticker)):
+            df = web.DataReader(ticker, 'yahoo', start, end)
+            df.to_csv('stock_dfs/{}.csv'.format(ticker))
+        else:
+            print('Already have {}'.format(ticker))
+
+
+# get_data_from_yahoo()
